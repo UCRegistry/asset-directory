04cbaf0b960f614e55333c1b2c1282f49b4c9378
Antoine Herzog <antoine@iov.one>
2020-11-27 15:25:59 +0100

    fix gitignore

--- format.js
+++ format.js
@@ -7,12 +7,8 @@ fs.readdirSync(".")
   .filter((dir) => fs.statSync(dir).isDirectory())
   .forEach((dir) => {
     const fileAsset = path.join(dir, "asset.json"); // HARD-CODED
-    const fileMetadata = path.join(dir, "metadata", "info.json"); // HARD-CODED
     const jsonAsset = fs.readFileSync(fileAsset, "utf-8");
-    const jsonMetadata = fs.readFileSync(fileMetadata, "utf-8");
     const asset = JSON.parse(jsonAsset);
-    const metadata = JSON.parse(jsonMetadata);
 
     fs.writeFileSync(fileAsset, stringify(asset, { space: "  " }) + "\n");
-    fs.writeFileSync(fileMetadata, stringify(metadata, { space: "  " }) + "\n");
   });
